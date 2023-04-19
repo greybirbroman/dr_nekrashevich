47cc66927e5f06e8ddabc725be61923c8f92b6f4
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2023-04-19 21:26:38 +0300

    fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 function Header() {
   return (
     <header
-      className='flex justify-between items-center py-5 px-5 md:py-3 sm:py-3 sm:px-3 absolute top-0 left-0 right-0'
+      className='flex justify-between items-center py-5 px-5 md:py-3 sm:py-3 sm:px-3 absolute top-0 left-0 right-0 z-10'
     >
       <Logo />
       <nav>
